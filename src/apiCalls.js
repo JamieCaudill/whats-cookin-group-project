d3a3419923939d1eb105d24797a88c551c817a3c
Parvin Sattorova <nanajon66@gmail.com>
2023-05-19 15:07:33 -0600

    feat:Add pseudocode

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,18 @@
 // Your fetch requests will live here!
 
 
-console.log('I will be a fetch request!')
+// console.log('I will be a fetch request!')
+
+
+// Pseudocode.
+// Define a function fetchApi that takes a URL parameter.
+// Inside fetchApi:
+//   Use the fetch function to make a GET request to the specified URL.
+//   If the response is successful (status code 200-299), return the JSON data.
+//   Otherwise, throw an error with the message 'No Such Path'.
+// Define a function fetchAllData.
+// Inside fetchAllData:
+//   Use Promise.all to make parallel requests to fetch data from the 'users', 'recipes', and 'ingredients' endpoints.
+//   Return a promise that resolves to an array of the fetched data.
+//   If there is an error during the fetching process, call the handleError function.
+
