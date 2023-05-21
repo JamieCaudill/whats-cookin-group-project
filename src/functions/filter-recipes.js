14cda1efb6fe9ffda45d8f376adbc86991c78ec6
Parvin Sattorova <nanajon66@gmail.com>
2023-05-20 21:38:40 -0600

    refactor: Add test case to filterRecipes when filter is not provided

--- src/functions/filter-recipes.js
+++ src/functions/filter-recipes.js
@@ -35,23 +35,55 @@ const filterByName = (recipeData, name) => {
   return filteredRecipesByName;
 };
 
+// const filterRecipes = (recipeData, filter) => {
+//   const recipes = [];
+//   const filteredByName = filterByName(recipeData, filter);
+//   const filteredByTag = filterByTag(recipeData, filter);
+  
+//   if(filteredByName !== 'No results') {
+//     filteredByName.forEach(recipe => recipes.push(recipe));
+//   }
+//   if(filteredByTag !== 'Error: try a new tag') {
+//     filteredByTag.forEach(recipe => recipes.push(recipe));
+//   }
+//   if(filter === 'all') {
+//     recipeData.forEach(recipe => recipes.push(recipe));
+//   }
+//   if (!filter) {
+//     return [];
+//   }
+//   let uniqueRecipes = [... new Set(recipes)];
+//   return uniqueRecipes;
+// };
+
 const filterRecipes = (recipeData, filter) => {
   const recipes = [];
-  const filteredByName = filterByName(recipeData, filter);
-  const filteredByTag = filterByTag(recipeData, filter);
+  const lowerCaseFilter = filter.toLowerCase();
+  console.log('parvin:', filter) 
   
-  if(filteredByName !== 'No results') {
-    filteredByName.forEach(recipe => recipes.push(recipe));
-  }
-  if(filteredByTag !== 'Error: try a new tag') {
-    filteredByTag.forEach(recipe => recipes.push(recipe));
-  }
-  if(filter === 'all') {
-    recipeData.forEach(recipe => recipes.push(recipe));
-  }
-  if (!filter) {
+  const searchTerms = lowerCaseFilter.split(' ');
+  console.log('parvin', searchTerms)   
+  
+  recipeData.forEach(recipe => {
+    const lowerCaseRecipeName = recipe.name.toLowerCase();
+    const recipeTags = recipe.tags.map(tag => tag.toLowerCase());
+    
+    const matchName = searchTerms.some(term => lowerCaseRecipeName.includes(term));
+   console.log('parvin:', matchName) 
+    
+    const matchTag = searchTerms.some(term => recipeTags.includes(term));
+    console.log('parvin:', matchTag) 
+    
+    if ((matchName || matchTag) || filter === 'all') {
+      recipes.push(recipe);
+    }
+    if (!filter) {
     return [];
   }
+  });
+  // if (recipes.length === 0) {
+  //   return 'No results';
+  // }
   let uniqueRecipes = [... new Set(recipes)];
   return uniqueRecipes;
 };
